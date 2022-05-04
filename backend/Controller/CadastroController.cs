c795855fc44c0f80f57e63a655b937c9f571a35d
Antonio Junio Pardim <antoniojrpardim@gmail.com>
2022-05-04 20:09:52 -0300

    Projeto Angular

---
+++ backend/Controller/CadastroController.cs
@@ -0,0 +1,42 @@
+﻿using backend.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Controller
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class CadastroController: ControllerBase
+  {
+    private static List<Cadastro> cadastros = new List<Cadastro>();
+    private static int id = 1;
+
+   [HttpPost]
+   public void adicionarCadastro([FromBody] Cadastro cadastro)
+    {
+      cadastro.Id = id++;
+      cadastros.Add(cadastro);
+      Console.WriteLine(cadastro.nome);
+    }
+    [HttpGet]
+    public List<Cadastro> receberTodosCadastros()
+    {
+      return cadastros;
+    }
+  
+    [HttpGet("{id}")]
+    public IActionResult retornaCadastroId(int id)
+    {
+      Cadastro cadastro = cadastros.FirstOrDefault(cadastro => cadastro.Id == id);
+      if(cadastro != null)
+      {
+        return Ok(cadastro);
+      }
+      return NotFound();
+      
+    }
+  }
+}
---
+++ backend/Model/Cadastro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Model
+{
+  public class Cadastro
+  {
+    
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O campo nome de usuario e obrigatorio")]
+    public string nome { get; set; }
+    [Required(ErrorMessage = "O email e obrigadorio")]
+    public string email { get; set; }
+    [Required(ErrorMessage = "A senha e Obrigadoria")]
+    public string senha { get; set; }
+  }
+}
